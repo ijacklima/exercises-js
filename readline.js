4df5c1bc1a27504c865f8bc7098851df709454b5
ijacklima <jacklimavas@gmail.com>
2022-06-08 19:36:52 -0300

    added readline exercise

---
+++ readline.js
@@ -0,0 +1,16 @@
+const { stdin } = require("process");
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question("What is your name?", function(name) {
+  console.log("Hello" + name + "!");
+  rl.close();
+});
+
+rl.on("close", function() {
+  console.log("\nSee you later!");
+  process.exit(0);
+});
